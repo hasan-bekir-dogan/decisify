533ab61ca155bf7bd6e7cdc4dde0536ff252c075
Hasan Bekir Doğan <hasan.bekir.dogan@hotmail.com>
2026-08-23 23:05:55 +0200

    Revert "feat(auth): configure JWT security filter chain"
    This reverts commit 018ab90e85aee0bbe7e26e9785df0715daa1b754.

--- services/auth-service/src/main/java/com/decisify/auth/exception/InvalidAccessTokenException.java
+++
@@ -1,8 +0,0 @@
-package com.decisify.auth.exception;
-
-public class InvalidAccessTokenException extends RuntimeException {
-
-    public InvalidAccessTokenException() {
-        super("Invalid or expired access token");
-    }
-}
